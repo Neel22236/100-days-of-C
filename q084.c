6442d139e826d27b05adc2d14ce72d1de679c226
Neel22236 <choudharyneel2005@gmail.com>
2025-10-24 22:38:31 +0530

    Update q084.c

--- q084.c
+++ q084.c
@@ -7,4 +7,26 @@ hello
 Output 1:
 HELLO
 
+
 */
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int i = 0;
+
+    printf("Enter a lowercase string: ");
+    fgets(str, sizeof(str), stdin); // Read the input string
+
+    // Convert to uppercase manually
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32; // Convert lowercase to uppercase
+        }
+        i++;
+    }
+
+    printf("String in uppercase: %s", str);
+
+    return 0;
+}
